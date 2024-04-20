87df08fd4015b2d30135b5e62c069bef11014398
fatyh86 <fatyh_fth@yahoo.com>
2024-04-20 21:53:48 +0300

    up

---
+++ src/app/admin/forms/components/new-forms/new-forms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-forms.component.html',
+  styleUrl: './new-forms.component.scss'
+})
+export class NewFormsComponent {
+
+}
